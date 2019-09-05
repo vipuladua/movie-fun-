8b72561071cac876527e2005c22ee308d7103ae3
bhneha <bh.neha@gmail.com>
2019-09-05 09:49:03 +0530

    replatforming Spring Bootification

--- src/main/java/org/superbiz/moviefun/MoviesBean.java
+++ src/main/java/org/superbiz/moviefun/MoviesBean.java
@@ -29,7 +29,7 @@ import java.util.List;
 @Repository
 public class MoviesBean {
 
-    @PersistenceContext()
+    @PersistenceContext
     private EntityManager entityManager;
 
     public Movie find(Long id) {
